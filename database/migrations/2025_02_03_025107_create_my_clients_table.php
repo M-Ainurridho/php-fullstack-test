518aba7ba71fb5825354150e3652f1e7e8f7d8a8
M-Ainurridho <m.ainurridho11@gmail.com>
2025-02-03 09:59:01 +0700

    atur orm unutuk table my_client

--- database/migrations/2025_02_03_025107_create_my_clients_table.php
+++ database/migrations/2025_02_03_025107_create_my_clients_table.php
@@ -11,9 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('my_clients', function (Blueprint $table) {
+        Schema::create('my_client', function (Blueprint $table) {
             $table->id();
+            $table->char('name', 250);
+            $table->char('slug', 100)->unique();
+            $table->enum('is_project', ['0', '1'])->default('0');
+            $table->char('self_capture', 1)->default('1');
+            $table->char('client_prefix', 4);
+            $table->char('client_logo', 255)->default('nophoto.jpg');
+            $table->text('address')->nullable();
+            $table->char('phone_number', 50)->nullable();
+            $table->char('city', 50)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
