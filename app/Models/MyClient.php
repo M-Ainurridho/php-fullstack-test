f7fa34d8d2c28cf85cf7fd0db6922faa631b2d50
M-Ainurridho <m.ainurridho11@gmail.com>
2025-02-03 10:20:19 +0700

    buat data yang bisa diinput/mass pada model myClient

--- app/Models/MyClient.php
+++ app/Models/MyClient.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MyClient extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'my_client';
+    protected $fillable = [
+        'name', 'slug', 'is_project', 'self_capture', 'client_prefix',
+        'client_logo', 'address', 'phone_number', 'city'
+    ];
 }
